9910d813c0d9161e19cf0efd7c5a955cc19b721c
ThaoNguyenNguyen <159298362+ThaoNguyenNguyen@users.noreply.github.com>
2024-05-20 22:06:31 +0700

    Add files via upload

---
+++ src/lab3/Login.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import { Appbar, Text, TextInput , Button  } from 'react-native-paper';
+import { login, useMyContextController } from './Store/Index';
+
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState('ngvanhungg1512@gmail.com');
+  const [password, setPassword] = useState('123456');
+  const [showPassword,setShowPassword]=useState(true)
+
+  const [controller,dispatch]=useMyContextController();
+  const {userLogin}=controller;
+
+  useEffect(()=>{
+    console.log(userLogin)
+    
+    if(userLogin!=null){
+      if(userLogin.role=="admin"){
+        navigation.navigate("Admin")
+      }else{
+        navigation.navigate("Customer")
+      }
+    }
+  },[userLogin])
+
+  const handleLogin = () => {
+    login(dispatch,email,password)
+  };
+
+  return (
+    <View style={{backgroundColor:'#f2f2f2',flex:1}}>
+      {/* <Appbar title="Đăng nhập" /> */}
+
+      <View style={{ padding: 20, marginTop:'50%'}}>
+        <Text variant='displayMedium' style={{textAlign:'center',color:'#ef506b', fontWeight:'bold',marginBottom:30}}>Login</Text>
+
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          style={{marginBottom:10}}
+        />
+
+        <TextInput
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={showPassword}
+          right={<TextInput.Icon icon={showPassword?"eye-off":"eye"} onPress={()=>setShowPassword(!showPassword)}/>}
+        />
+        <View style={{flexDirection:'row',marginTop:10}}>
+            <TouchableOpacity onPress={()=>{navigation.navigate("ForgotPassword")}}>
+                <Text style={{color:'#ef506b',fontWeight:'bold',textAlign:'right'}}> Forgot password?</Text>
+            </TouchableOpacity>
+        </View>
+        <Button style={{backgroundColor:'#ef506b',marginTop:20,height:40,borderRadius:8}} onPress={handleLogin}>
+            <Text style={{color:'white'}}>Login</Text>
+        </Button>
+        <View style={{flexDirection:'row',justifyContent:'center',marginTop:10}}>
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')} >
+                <Text style={{color:'#ef506b',fontWeight:'bold'}}> Sign up</Text>
+            </TouchableOpacity>
+        </View>
+
+      </View>
+    </View>
+  );
+};
+
+export default Login;
